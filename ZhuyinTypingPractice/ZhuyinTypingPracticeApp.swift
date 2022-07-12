dbb4fefcfa54e617cddd02da961c41a2cc721a7d
Matthew <matthewmclaughlin508@me.com>
2022-07-12 14:05:12 -0700

    Initial Commit

---
+++ ZhuyinTypingPractice/ZhuyinTypingPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZhuyinTypingPracticeApp.swift
+//  ZhuyinTypingPractice
+//
+//  Created by Matthew on 7/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ZhuyinTypingPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
